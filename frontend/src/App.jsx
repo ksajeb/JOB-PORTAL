96c62735ec7457b0d33bb76b0a0ff0ec4fc5fc61
SAJEB KHAN <sajebk007@gmail.com>
2024-10-01 21:31:57 +0530

    Design Browse page and job page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Login from "./components/auth/Login";
 import Signup from "./components/auth/Signup";
 import Home from "./components/Home";
 import Jobs from "./components/Jobs";
+import Browse from "./components/Browse";
 
 //4.14 hrs
 
@@ -23,7 +24,10 @@ const appRouter = createBrowserRouter([
     path: "/jobs",
     element: <Jobs />,
   },
-
+  {
+    path: "/browse",
+    element: <Browse />,
+  },
 ]);
 
 function App() {
